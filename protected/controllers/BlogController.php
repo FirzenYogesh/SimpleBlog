c8eda48cdedcac6c164067b96e2158bfc982401c
Yogesh S <contact@syogesh.in>
2017-12-22 17:32:40 +0530

    Update: Added API for Update Post and Comment

--- protected/controllers/BlogController.php
+++ protected/controllers/BlogController.php
@@ -1,93 +1,135 @@
 <?php
 
-class BlogController extends Controller
-{
-	public function actionIndex()
-	{
+class BlogController extends Controller {
+	public function actionIndex() {
 		$criteria = new CDbCriteria;
-		$criteria->select = array('id','author','title','category_id');
-		$posts=PostsTable::model()->findAll($criteria);
+		$criteria->select = array('id', 'author', 'title', 'category_id');
+		$posts = PostsTable::model()->findAll($criteria);
 		$data = array();
-		foreach ($posts as $p) 
-		{
+		foreach ($posts as $p) {
 			$arr = array(
 				'id' => $p->id,
 				'author' => $p->author,
 				'title' => $p->title,
-				'category_id' => $p->category_id 
-			);	
+				'category_id' => $p->category_id,
+			);
 			array_push($data, $arr);
 		}
 		echo CJSON::encode($data);
 	}
 
-	public function actionPostView($id)
-	{
-		$post=PostsTable::model()->findByPk($id);
+	public function actionViewPost($id) {
+		$post = PostsTable::model()->findByPk($id);
 		$data = array(
-			'id'=>$post->id,
-			'author'=>$post->author,
-			'title'=>$post->title,
-			'category_id'=>$post->category_id
+			'id' => $post->id,
+			'author' => $post->author,
+			'title' => $post->title,
+			'category_id' => $post->category_id,
 		);
 		$data['comments'] = array();
-		foreach ($post->comments as $c)
-		{
+		foreach ($post->comments as $c) {
 			$arr = array(
-				'id'=> $c->id,
-				'comment'=> $c->comment,
-				'author'=> $c->author
+				'id' => $c->id,
+				'comment' => $c->comment,
+				'author' => $c->author,
 			);
-			array_push($data['comments'],$arr);
+			array_push($data['comments'], $arr);
 		}
 		echo CJSON::encode($data);
 	}
 
-	public function actionPostCreate()
-	{
+	public function actionCreatePost() {
 		$retstatus = array();
 		$post = PostsTable::create(array(
-				"author"=>$_GET['author'],
-				"title"=>$_GET['title'],
-				"content"=>$_GET['content'],
-				"category_id"=>$_GET['category_id']
-			));
-		if($post->hasErrors(NULL))
-		{
+			"author" => $_GET['author'],
+			"title" => $_GET['title'],
+			"content" => $_GET['content'],
+			"category_id" => $_GET['category_id'],
+		));
+		if ($post->hasErrors(NULL)) {
 			$retstatus['statusCode'] = 0;
 			$retstatus['statusMessage'] = array();
 			foreach ($post->getErrors() as $key => $value) {
 				$arr = array($key => $value);
 				array_push($retstatus['statusMessage'], $arr);
 			}
-		}
-		else
-		{
-			
+		} else {
+
 			$retstatus['statusCode'] = 1;
 			$retstatus['statusMessage'] = "Blog Post Successful";
 		}
 		echo json_encode($retstatus);
-		
+
 	}
 
-	public function actionCommentCreate()
-	{
+	public function actionUpdatePost($id) {
 		$retstatus = array();
-		$post = CommentsTable::create(array(
-			"author"=>$_GET['author'],
-			"comment"=>$_GET['comment'],
-			"post_id"=>$_GET['post_id']
-		));
-		if($post->hasErrors(NULL))
-		{
+		$post = PostsTable::model()->findByPk($id);
+		if (isset($_GET['content'])) {
+			$post->updateColumns(array(
+				'content' => $_GET['content'],
+			));
+			if (!$post->hasErrors(NULL)) {
+				$retstatus['statusCode'] = 1;
+				$retstatus['statusMessage'] = "Update Content Successful";
+			}
+		}
+		if (isset($_GET['category_id'])) {
+			$post->updateColumns(array(
+				'category_id' => $_GET['category_id'],
+			));
+			if (!$post->hasErrors(NULL)) {
+				$retstatus['statusCode'] = 1;
+				$retstatus['statusMessage'] = "Update Category Successful";
+			}
+		}
+		if (isset($_GET['content']) && isset($_GET['category_id'])) {
+			if (!$post->hasErrors(NULL)) {
+				$retstatus['statusCode'] = 1;
+				$retstatus['statusMessage'] = "Update Post Successful";
+			}
+		}
+		if ($post->hasErrors(NULL)) {
 			$retstatus['statusCode'] = 0;
 			$retstatus['statusMessage'] = CJSON::encode($post->getErrors(NULL));
 		}
-		else
-		{
+		echo json_encode($retstatus);
+	}
+
+	public function actionCreateComment() {
+		$retstatus = array();
+		$post = CommentsTable::create(array(
+			"author" => $_GET['author'],
+			"comment" => $_GET['comment'],
+			"post_id" => $_GET['post_id'],
+		));
+		if ($post->hasErrors(NULL)) {
+			$retstatus['statusCode'] = 0;
+			$retstatus['statusMessage'] = CJSON::encode($post->getErrors(NULL));
+		} else {
 			$retstatus['statusCode'] = 1;
 			$retstatus['statusMessage'] = "Comment Post Successful";
+		}
+		echo json_encode($retstatus);
+	}
+
+	public function actionUpdateComment($id) {
+		$retstatus = array();
+		$post = CommentsTable::model()->findByPk($id);
+		if (isset($_GET['comment'])) {
+			$post->updateColumns(array(
+				'comment' => $_GET['comment'],
+			));
+			if ($post->hasErrors(NULL)) {
+				$retstatus['statusCode'] = 0;
+				$retstatus['statusMessage'] = CJSON::encode($post->getErrors(NULL));
+			} else {
+				$retstatus['statusCode'] = 1;
+				$retstatus['statusMessage'] = "Update Comment Successful";
+			}
+		} else {
+			$retstatus['statusCode'] = 1;
+			$retstatus['statusMessage'] = "No Changes were given";
 		}
 		echo json_encode($retstatus);
 	}
--- protected/models/PostsTable.php
+++ protected/models/PostsTable.php
@@ -12,139 +12,132 @@
  * @property integer $created_at
  * @property integer $updated_at
  */
-class PostsTable extends CActiveRecord
-{
-    /**
-     * @return string the associated database table name
-     */
-    public function tableName()
-    {
-        return 'posts_table';
-    }
+class PostsTable extends CActiveRecord {
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName() {
+		return 'posts_table';
+	}
 
-    /**
-     * @return array validation rules for model attributes.
-     */
-    public function rules()
-    {
-        // NOTE: you should only define rules for those attributes that
-        // will receive user inputs.
-        /*return array(
-            array('author, title, content, created_at, updated_at', 'required'),
-            array('category_id, created_at, updated_at', 'numerical', 'integerOnly'=>true),
-            array('author, title', 'length', 'max'=>255),
-            // The following rule is used by search().
-            // @todo Please remove those attributes that should not be searched.
-            array('id, author, title, content, category_id, created_at, updated_at', 'safe', 'on'=>'search'),
-        );*/
-        return array(
-            array('author, title, content', 'required'),
-            array('category_id', 'numerical', 'integerOnly'=>true),
-            array('author, title', 'length', 'max'=>255),
-            array('title','checkUniqueness'),
-            // The following rule is used by search().
-            // @todo Please remove those attributes that should not be searched.
-            array('id, author, title, category_id', 'safe', 'on'=>'search'),
-        );
-    }
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules() {
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		/*return array(
+	            array('author, title, content, created_at, updated_at', 'required'),
+	            array('category_id, created_at, updated_at', 'numerical', 'integerOnly'=>true),
+	            array('author, title', 'length', 'max'=>255),
+	            // The following rule is used by search().
+	            // @todo Please remove those attributes that should not be searched.
+	            array('id, author, title, content, category_id, created_at, updated_at', 'safe', 'on'=>'search'),
+*/
+		return array(
+			array('author, title, content', 'required'),
+			array('category_id', 'numerical', 'integerOnly' => true),
+			array('author, title', 'length', 'max' => 255),
+			array('title', 'checkUniqueness'),
+			// The following rule is used by search().
+			// @todo Please remove those attributes that should not be searched.
+			array('id, author, title, category_id', 'safe', 'on' => 'search'),
+		);
+	}
 
-    /**
-     * @return array relational rules.
-     */
-    public function relations()
-    {
-        // NOTE: you may need to adjust the relation name and the related
-        // class name for the relations automatically generated below.
-        return array('comments'=>array(self::HAS_MANY,'CommentsTable', 'post_id'));
-    }
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations() {
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array('comments' => array(self::HAS_MANY, 'CommentsTable', 'post_id'));
+	}
 
-    /**
-     * @return array customized attribute labels (name=>label)
-     */
-    public function attributeLabels()
-    {
-        return array(
-            'id' => 'ID',
-            'author' => 'Author',
-            'title' => 'Title',
-            'content' => 'Content',
-            'category_id' => 'Category',
-            'created_at' => 'Created At',
-            'updated_at' => 'Updated At',
-            'comments' => 'Comments'
-        );
-    }
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels() {
+		return array(
+			'id' => 'ID',
+			'author' => 'Author',
+			'title' => 'Title',
+			'content' => 'Content',
+			'category_id' => 'Category',
+			'created_at' => 'Created At',
+			'updated_at' => 'Updated At',
+			'comments' => 'Comments',
+		);
+	}
 
-    /**
-     * Retrieves a list of models based on the current search/filter conditions.
-     *
-     * Typical usecase:
-     * - Initialize the model fields with values from filter form.
-     * - Execute this method to get CActiveDataProvider instance which will filter
-     * models according to data in model fields.
-     * - Pass data provider to CGridView, CListView or any similar widget.
-     *
-     * @return CActiveDataProvider the data provider that can return the models
-     * based on the search/filter conditions.
-     */
-    public function search()
-    {
-        // @todo Please modify the following code to remove attributes that should not be searched.
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 *
+	 * Typical usecase:
+	 * - Initialize the model fields with values from filter form.
+	 * - Execute this method to get CActiveDataProvider instance which will filter
+	 * models according to data in model fields.
+	 * - Pass data provider to CGridView, CListView or any similar widget.
+	 *
+	 * @return CActiveDataProvider the data provider that can return the models
+	 * based on the search/filter conditions.
+	 */
+	public function search() {
+		// @todo Please modify the following code to remove attributes that should not be searched.
 
-        $criteria=new CDbCriteria;
+		$criteria = new CDbCriteria;
 
-        $criteria->compare('id',$this->id);
-        $criteria->compare('author',$this->author,true);
-        $criteria->compare('title',$this->title,true);
-        $criteria->compare('category_id',$this->category_id);
+		$criteria->compare('id', $this->id);
+		$criteria->compare('author', $this->author, true);
+		$criteria->compare('title', $this->title, true);
+		$criteria->compare('category_id', $this->category_id);
 
-        return new CActiveDataProvider($this, array(
-            'criteria'=>$criteria,
-        ));
-    }
+		return new CActiveDataProvider($this, array(
+			'criteria' => $criteria,
+		));
+	}
 
-    /**
-     * Returns the static model of the specified AR class.
-     * Please note that you should have this exact method in all your CActiveRecord descendants!
-     * @param string $className active record class name.
-     * @return PostsTable the static model class
-     */
-    public static function model($className=__CLASS__)
-    {
-        return parent::model($className);
-    }
+	/**
+	 * Returns the static model of the specified AR class.
+	 * Please note that you should have this exact method in all your CActiveRecord descendants!
+	 * @param string $className active record class name.
+	 * @return PostsTable the static model class
+	 */
+	public static function model($className = __CLASS__) {
+		return parent::model($className);
+	}
 
-    public function primaryKey()
-    {
-        return 'id';
-    }
+	public function primaryKey() {
+		return 'id';
+	}
 
-    public function beforeSave() {
-        if($this->isNewRecord) { 
-            $this->created_at = time();
-        }
-        $this->updated_at = time();
-        return parent::beforeSave();
-    }
+	public function beforeSave() {
+		if ($this->isNewRecord) {
+			$this->created_at = time();
+		}
+		$this->updated_at = time();
+		return parent::beforeSave();
+	}
 
-    public function updateColumns($column_value_array) {
-        $column_value_array['updated_at'] = time();
-        foreach($column_value_array as $column_name => $column_value)
-            $this->$column_name = $column_value;
-        $this->update(array_keys($column_value_array));
-    }
+	public function updateColumns($column_value_array) {
+		$column_value_array['updated_at'] = time();
+		foreach ($column_value_array as $column_name => $column_value) {
+			$this->$column_name = $column_value;
+		}
+		$this->update(array_keys($column_value_array));
+	}
 
-    public static function create($attributes) {
-        $model = new PostsTable;
-        $model->attributes = $attributes;
-        $model->save();
-        return $model;
-    }
-    
-    public function checkUniqueness($attribute, $params)
-    {
-        $model = PostsTable::model()->find('title=:title', array(':title'=>$this->title));
-        if($model != null)
-            $this->addError('title',"This Title already exists");
-    }
+	public static function create($attributes) {
+		$model = new PostsTable;
+		$model->attributes = $attributes;
+		$model->save();
+		return $model;
+	}
+
+	public function checkUniqueness($attribute, $params) {
+		$model = PostsTable::model()->find('title=:title', array(':title' => $this->title));
+		if ($model != null) {
+			$this->addError('title', "This Title already exists");
+		}
+	}
 }
